b94ccccf9bdc3ccb9efab5541408c5526675957f
mdsauce <max.dobeck@saucelabs.com>
2019-08-30 10:23:15 -0500

    Started adding baseline for --pretty tunnel output

--- output/pretty.go
+++ output/pretty.go
@@ -9,15 +9,27 @@ import (
 // humanOutput takes the last known state for the tunnels and outputs it in a concise & readable way
 func prettyOutputTunnel(id string) {
 	state := manager.GetLastKnownState()
-	fmt.Println(state)
+	if state.Empty() == true {
+		fmt.Println("0 tunnels found by Sauced!")
+	} else {
+		fmt.Println(state)
+	}
 }
 
 func prettyOutputPool(pool string) {
 	state := manager.GetLastKnownState()
-	fmt.Println(state)
+	if state.Empty() == true {
+		fmt.Println("0 tunnels found by Sauced!")
+	} else {
+		fmt.Println(state)
+	}
 }
 
 func prettyOutputState() {
 	state := manager.GetLastKnownState()
-	fmt.Println(state)
+	if state.Empty() == true {
+		fmt.Println("0 tunnels found by Sauced!")
+	} else {
+		fmt.Println(state)
+	}
 }
