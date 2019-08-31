5abad08fafd39d493bdfe181b994872357f0a7a0
mdsauce <max.dobeck@saucelabs.com>
2019-08-30 22:18:38 -0700

    Added basic printTunnel func that can be used to pretty print tunnels once a single tunnel or slice of tunnels is avavailable.

--- output/pretty.go
+++ output/pretty.go
@@ -3,6 +3,7 @@ package output
 import (
 	"fmt"
 
+	"github.com/mdsauce/sauced/logger"
 	"github.com/mdsauce/sauced/manager"
 )
 
@@ -11,7 +12,11 @@ func showTunnelPretty(ID string, state manager.LastKnownTunnels) {
 		noTunnels()
 		return
 	}
-
+	tunnel, err := state.FindTunnelsByID(ID)
+	if err != nil {
+		logger.Disklog.Warn("Problem searching Statefile for tunnel", " , ", ID, err)
+	}
+	printTunnel(tunnel)
 }
 
 func showPoolPretty(pool string, state manager.LastKnownTunnels) {
@@ -33,4 +38,18 @@ func noTunnels() {
 	fmt.Println("Tunnels:")
 	fmt.Println("--------")
 	fmt.Print("None\n\n")
+}
+
+func printTunnel(t manager.Tunnel) {
+	fmt.Println("\nTunnel Found")
+	fmt.Println("------------")
+	fmt.Println("PID: ", t.PID)
+	fmt.Println("Assigned ID: ", t.AssignedID)
+	fmt.Println("Name: ", t.Metadata.Pool)
+	fmt.Println("Tunnel Log Location: ", t.Log)
+	//convert to local OS timezone and humanize
+	// t.LaunchTime
+	fmt.Println("Owner: ", t.Metadata.Owner)
+	fmt.Println("Pool Size: ", t.Metadata.Size)
+	fmt.Println()
 }
