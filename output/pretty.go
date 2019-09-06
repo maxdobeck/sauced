c4b5a96ef4997cda961069f1d4002fec744db71c
mdsauce <max.dobeck@saucelabs.com>
2019-09-06 16:54:52 -0700

    Added local time detection in pretty.go for timezone output.

--- output/pretty.go
+++ output/pretty.go
@@ -2,6 +2,7 @@ package output
 
 import (
 	"log"
+	"time"
 
 	"github.com/mdsauce/sauced/logger"
 	"github.com/mdsauce/sauced/manager"
@@ -57,8 +58,15 @@ func printTunnel(t manager.Tunnel) {
 	log.Println("Assigned ID: ", t.AssignedID)
 	log.Println("Pool Name: ", t.Metadata.Pool)
 	log.Println("Tunnel Log Location: ", t.Log)
-	//convert to local OS timezone and humanize
-	// t.LaunchTime
+	if time.Local == nil {
+		// alternative hard coding to PST.  Use this if something breaks with t.local
+		cali, _ := time.LoadLocation("America/Los_Angeles")
+		local := t.LaunchTime.In(cali)
+		log.Println("Launch Time (PST zone 24-hr):", local.Format("15:04:05 Mon Jan 2 2006"))
+	} else {
+		local := t.LaunchTime.In(time.Local)
+		log.Println("Launch Time (local 24-hr):", local.Format("15:04:05 Mon Jan 2 2006"))
+	}
 	log.Println("Owner: ", t.Metadata.Owner)
 	log.Print("Pool Size: ", t.Metadata.Size, "\n\n")
 }
