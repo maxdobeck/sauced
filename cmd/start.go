8ef60deca69ba463ad380525a517fef457f0d36d
mdsauce <max.dobeck@saucelabs.com>
2019-09-06 17:28:14 -0700

    Small working solution that doesn't call REST API or shut down gracefully but will stopall.  not sure why StopAll() is not gracefully shutting down.

--- cmd/start.go
+++ cmd/start.go
@@ -17,6 +17,7 @@ package cmd
 import (
 	"bufio"
 	"os"
+	"os/signal"
 	"path"
 	"sync"
 
@@ -63,6 +64,7 @@ var startCmd = &cobra.Command{
 		// read in the sc startup commands
 		file, _ := os.Open(configFile)
 		fscanner := bufio.NewScanner(file)
+		c := make(chan os.Signal, 1)
 		for fscanner.Scan() {
 			if fscanner.Text() != "" || len(fscanner.Text()) != 0 {
 				wg.Add(1)
@@ -73,7 +75,12 @@ var startCmd = &cobra.Command{
 				go manager.Start(fscanner.Text(), &wg, meta[pool])
 			}
 		}
-		// TODO catch the ctrl-c here if it comes.
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			<-c
+			manager.StopAll()
+			os.Exit(0)
+		}()
 		wg.Wait()
 	},
 }
