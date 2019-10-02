a8ae5298c9d51b05e5689bf19520abaa347a742d
mdsauce <max.dobeck@saucelabs.com>
2019-10-02 15:57:46 -0700

    Removed default setting from the parser.  Wasn't applicable

--- manager/parser_unix.go
+++ manager/parser_unix.go
@@ -2,10 +2,7 @@ package manager
 
 import (
 	"math/rand"
-	"os"
 	"strings"
-
-	"github.com/mdsauce/sauced/logger"
 )
 
 func eatLine(line string) bool {
@@ -14,92 +11,6 @@ func eatLine(line string) bool {
 		return true
 	}
 	return false
-}
-
-func setDefaults(args []string) []string {
-	args = addDefaults(args)
-
-	return args
-}
-
-func addDefaults(tunnelArgs []string) []string {
-	verbose, username, key, sePort, pidfile, haMode, logfile := false, false, false, false, false, false, false
-	for _, arg := range tunnelArgs {
-		switch arg {
-		case "-v":
-			verbose = true
-		case "--verbose":
-			verbose = true
-		case "-vv":
-			verbose = true
-		case "-u":
-			username = true
-		case "--user":
-			username = true
-		case "-k":
-			key = true
-		case "--api-key":
-			key = true
-		case "--se-port":
-			sePort = true
-		case "-P":
-			sePort = true
-		case "--pidfile":
-			pidfile = true
-		case "-d":
-			pidfile = true
-		case "--no-remove-colliding-tunnels":
-			haMode = true
-		case "-l":
-			logfile = true
-		case "--logfile":
-			logfile = true
-		}
-	}
-	_, noFile := os.Stat("/tmp/sauceconnect")
-	if noFile != nil {
-		if os.IsNotExist(noFile) {
-			err := os.Mkdir("/tmp/sauceconnect", 0744)
-			if err != nil {
-				logger.Disklog.Error("Could not make default directory /tmp/sauceconnect: ", err)
-			}
-		}
-	} else if noFile == nil {
-		logger.Disklog.Debug("Found /tmp/sauceconnect.  Adding Defaults to launch args")
-	}
-
-	if !verbose {
-		tunnelArgs = append(tunnelArgs, "-v")
-	}
-	if !username {
-		sauceuser := os.Getenv("SAUCE_USERNAME")
-		tunnelArgs = append(tunnelArgs, "-u")
-		tunnelArgs = append(tunnelArgs, sauceuser)
-	}
-	if !key {
-		saucekey := os.Getenv("SAUCE_ACCESS_KEY")
-		tunnelArgs = append(tunnelArgs, "-k")
-		tunnelArgs = append(tunnelArgs, saucekey)
-	}
-	if !sePort {
-		tunnelArgs = append(tunnelArgs, "--se-port")
-		tunnelArgs = append(tunnelArgs, "0")
-	}
-	if !pidfile {
-		tunnelArgs = append(tunnelArgs, "--pidfile")
-		pidfileString := "/tmp/sauceconnect/" + randomString(5) + ".pid"
-		tunnelArgs = append(tunnelArgs, pidfileString)
-	}
-	if !haMode {
-		tunnelArgs = append(tunnelArgs, "--no-remove-colliding-tunnels")
-	}
-	if !logfile {
-		tunnelArgs = append(tunnelArgs, "--logfile")
-		rndLogfile := "/tmp/sauceconnect/" + randomString(5) + ".log"
-		tunnelArgs = append(tunnelArgs, rndLogfile)
-	}
-
-	return tunnelArgs
 }
 
 func missingRequiredFlags(args []string) bool {
