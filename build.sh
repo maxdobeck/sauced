4f7339c3f6cdbafa60117c1d06815880a57ab2b8
mdsauce <max.dobeck@saucelabs.com>
2019-10-04 16:14:05 -0700

    added zips for deploy in build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo ">>>>>Ignore any errors.  Not all versions will build properly<<<<<\n"
 rm -f ./sauced
+mkdir -p ./builds/zips
 
 echo "Building for Windows"
 env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/mdsauce/sauced/cmd.CurVersion=`git rev-parse --short HEAD`" -o sauced
@@ -12,10 +13,14 @@ echo "Building for Linux"
 env GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/mdsauce/sauced/cmd.CurVersion=`git rev-parse --short HEAD`" -o sauced
 mkdir -p ./builds/sauced-linux
 mv sauced ./builds/sauced-linux
+tar cvzf sauced-linux.tar.gz  ./builds/sauced-linux/
+mv sauced-linux.tar.gz ./builds/zips
 echo "done\n"
 
 echo "Building for Mac"
 env GOOS=darwin GOARCH=amd64 go build -ldflags "-X github.com/mdsauce/sauced/cmd.CurVersion=`git rev-parse --short HEAD`" -o sauced
 mkdir -p ./builds/sauced-mac
 mv sauced ./builds/sauced-mac
+tar cvzf sauced-mac.tar.gz  ./builds/sauced-mac/
+mv sauced-mac.tar.gz ./builds/zips
 echo "done\n"
