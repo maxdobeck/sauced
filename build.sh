5d2b38719b971d09ad6cf3c496c77d68774f2a24
mdsauce <max.dobeck@saucelabs.com>
2019-10-04 15:47:03 -0700

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
-rm ./sauced
+#!/bin/bash
+echo ">>>>>Ignore any errors.  Not all versions will build properly<<<<<\n"
+rm -f ./sauced
 
+echo "Building for Windows"
 env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/mdsauce/sauced/cmd.CurVersion=`git rev-parse --short HEAD`" -o sauced
-mkdir -p builds/sauced-windows
-mv sauced builds/sauced-windows
+mkdir -p ./builds/sauced-windows
+mv sauced ./builds/sauced-windows
+echo "done\n"
 
+echo "Building for Linux"
 env GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/mdsauce/sauced/cmd.CurVersion=`git rev-parse --short HEAD`" -o sauced
-mkdir -p builds/sauced-linux
-mv sauced builds/sauced-linux
+mkdir -p ./builds/sauced-linux
+mv sauced ./builds/sauced-linux
+echo "done\n"
 
-
-go build -ldflags "-X github.com/mdsauce/sauced/cmd.CurVersion=`git rev-parse --short HEAD`" -o sauced
-mkdir -p builds/sauced-mac
-mv sauced builds/sauced-mac
+echo "Building for Mac"
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-X github.com/mdsauce/sauced/cmd.CurVersion=`git rev-parse --short HEAD`" -o sauced
+mkdir -p ./builds/sauced-mac
+mv sauced ./builds/sauced-mac
+echo "done\n"
