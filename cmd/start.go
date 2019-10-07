11b2f8314e01a25c863c7072cf865991f5232aac
mdsauce <max.dobeck@saucelabs.com>
2019-10-07 16:11:49 -0700

    Rough working solution to split the Start function out based on the name of the file.

--- cmd/start.go
+++ cmd/start.go
@@ -20,6 +20,7 @@ import (
 	"os"
 	"os/signal"
 	"path"
+	"strings"
 	"sync"
 	"syscall"
 
@@ -39,62 +40,55 @@ var startCmd = &cobra.Command{
 	Short: "Start all tunnels listed in your config file you reference.",
 	Long:  `Start all tunnels in the config file you reference like $ sauced start ~/my-config.txt`,
 	Run: func(cmd *cobra.Command, args []string) {
-		configFile, err := cmd.Flags().GetString("config")
-		if err != nil {
-			logger.Disklog.Warn("Problem retrieving config file flag", err)
-		}
+		launch(configFile)
 
-		if !configUsable(configFile) {
-			logger.Disklog.Warn("You did not specify a config file! Please pass in a file like 'sauced start --config /path/to/sauced-config.txt'")
-			logger.Disklog.Debug("Checking for config file in any XDG_HOME_CONFIG environment variables.")
-			configFile = findXdgConfigHome()
+		if viper.ConfigFileUsed() == "" {
 
-			if len(configFile) < 2 {
-				logger.Disklog.Error("Problem retrieving config file flag.")
-				os.Exit(1)
+			if !configUsable(configFile) {
+				logger.Disklog.Warn("You did not specify a config file! Pass in a file like 'sauced start --config /path/to/sauced-config.txt'")
+				logger.Disklog.Debug("Checking for config file in any XDG_HOME_CONFIG environment variables.")
+				configFile = findXdgConfigHome()
 
+				if len(configFile) < 2 {
+					logger.Disklog.Error("Problem retrieving config file flag.")
+					os.Exit(1)
+
+				}
 			}
-		}
-
-		manager.PruneState()
-		meta := manager.CollectMetadata(configFile)
-
-		var wg sync.WaitGroup
-		// read in the sc startup commands
-		file, _ := os.Open(configFile)
-		fscanner := bufio.NewScanner(file)
-		stop := make(chan os.Signal, 1)
-		for fscanner.Scan() {
-			if fscanner.Text() != "" || len(fscanner.Text()) != 0 {
-				wg.Add(1)
-				pool := manager.PoolName(fscanner.Text())
-				logger.Disklog.Debugf("%s pool is %s.  Metadata is %v", fscanner.Text(), pool, meta[pool])
-				go manager.Start(fscanner.Text(), &wg, meta[pool])
+			if !strings.Contains(configFile, ".config") {
+				logger.Disklog.Error("Config file is not a .config: ", configFile)
 			}
+
+			manager.PruneState()
+			meta := manager.CollectMetadata(configFile)
+
+			var wg sync.WaitGroup
+			// read in the sc startup commands
+			file, _ := os.Open(configFile)
+			fscanner := bufio.NewScanner(file)
+			stop := make(chan os.Signal, 1)
+			for fscanner.Scan() {
+				if fscanner.Text() != "" || len(fscanner.Text()) != 0 {
+					wg.Add(1)
+					pool := manager.PoolName(fscanner.Text())
+					logger.Disklog.Debugf("%s pool is %s.  Metadata is %v", fscanner.Text(), pool, meta[pool])
+					go manager.Start(fscanner.Text(), &wg, meta[pool])
+				}
+			}
+			signal.Notify(stop, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+			go func() {
+				<-stop
+				logger.Disklog.Warn("User pressed CTRL-C (SIGINT). Killing tunnels now.  Active jobs using these tunnels may die.")
+			}()
+			wg.Wait()
 		}
-		signal.Notify(stop, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-		go func() {
-			<-stop
-			logger.Disklog.Warn("User pressed CTRL-C (SIGINT). Killing tunnels now.  Active jobs using these tunnels may die.")
-		}()
-		wg.Wait()
 	},
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.AddCommand(startCmd)
-	startCmd.PersistentFlags().StringP("config", "c", "", "config file for tunnels to start.")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVar(&configFile, "config", "", "config file for tunnels to start.")
 }
 
 func initConfig() {
@@ -109,22 +103,23 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".sauced" (without extension).
+		viper.SetConfigName("sauced")
+		xdgHome, _ := os.LookupEnv("XDG_CONFIG_HOME")
 		viper.SetConfigType("toml")
 		viper.AddConfigPath(home)
+		viper.AddConfigPath(xdgHome)
 		viper.AddConfigPath("$HOME/.config")
 		viper.AddConfigPath("$HOME/.config/sauced")
 		viper.AddConfigPath("$HOME/.sauced/config")
 		viper.AddConfigPath("$HOME/.sauced")
-		//check for XDG_CONFIG_HOME somewhere in here
-		viper.SetConfigName("sauced")
-		viper.SetConfigName("default")
-		// call this AFTER setting paths
-		viper.WatchConfig()
 	}
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			logger.Disklog.Info("No 'default' or 'sauced' config file found. ", err)
+		}
+	} else if err == nil {
 		logger.Disklog.Debug("Using config file:", viper.ConfigFileUsed())
 	} else {
 		logger.Disklog.Error("Problem reading config file: ", err)
@@ -175,4 +170,19 @@ func findXdgConfigHome() string {
 	configFile := xdgConfigPath
 
 	return configFile
+}
+
+func launch(config string) {
+	if strings.Contains(config, ".yaml") {
+		logger.Disklog.Error("YAML config files are not supported at this time.  Cannot use config file: ", configFile)
+	}
+
+	if strings.Contains(viper.ConfigFileUsed(), ".toml") || strings.Contains(config, ".toml") {
+		if viper.ConfigFileUsed() != "" {
+			if !configUsable(viper.ConfigFileUsed()) {
+				os.Exit(1)
+			}
+			logger.Disklog.Debug("Found TOML config file: ", viper.ConfigFileUsed())
+		}
+	}
 }
